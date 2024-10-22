cd3ec7e8ba1b606ed99f9506be95fa9b8a89afa9
Barbara Eguche <barbaraeguche@icloud.com>
2024-10-22 14:40:56 -0400

    adding all functions to 1 class

---
+++ src/Functions.java
@@ -0,0 +1,33 @@
+
+public class Functions {
+
+    //no need for default constructor as java already defines it if not included
+
+    public int StdDeviation() {
+        return 1;
+    }
+
+    public int XPowerY(float x, int y) {
+        return 1;
+    }
+
+    public int ABPowerX(int a, int b, int x) {
+        return 1;
+    }
+
+    public int MeanAbsDeviation() {
+        return 1;
+    }
+
+    public int ArcCosX(int x) {
+        return 1;
+    }
+
+    public int LogXBaseB(float x, int b) {
+        return 1;
+    }
+
+    public int HypSineX(int x) {
+        return 1;
+    }
+}
